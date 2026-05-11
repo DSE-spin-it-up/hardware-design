5b14c93d7b81522bc551fa925be837a6e992c59e
mattiz <mwacornelis@tudelft.nl>
2026-05-11 08:59:43 +0200

    Start structural analysis of the wing

---
+++ Structure.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import initial_sizing as si
+
+CL = si.CL # [-]
+q = si.q_cruise # [kg/ms^2]
+Sw = si.Sw # [m^2]
+L = CL * q * Sw # [N]
+
+cw = si.c # [m]
+tc = si.t_over_c_root # [-]
+tw = cw * tc # [m]
+
+dens_cfrp = 1.55 # [g/cm^3]
+rho_cfrp = dens_cfrp / 10**3 * 10**6 # [kg/m^3]
