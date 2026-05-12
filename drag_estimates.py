30cc8a749ba01052010dbb4e18e728b6cd0f221f
pholleboom <philip.holleboom@gmail.com>
2026-05-12 13:54:54 +0200

    add Vh/V

--- drag_estimates.py
+++ drag_estimates.py
@@ -43,11 +43,13 @@ max_tc_tail, max_tc_loc_tail = tail_airfoil.compute_maximum_thickness()
 # Flight parameters
 # ------------------------------
 mu_air = 1.82e-5  # dynamic viscosity
-lam = 0           # sweep angle
+lam_wing = 0           # sweep angle
+lam_tail = 0           # sweep angle
+V_tail = 0.85 * V_cruise  # tail speed (assumed to be lower than wing speed due to downwash)
 
 # Reynolds numbers
 Re_wing = rho_air * V_cruise * C_wing / mu_air
-Re_tail = rho_air * V_cruise * C_tail / mu_air
+Re_tail = rho_air * V_tail * C_tail / mu_air
 
 # Mach number
 M_cruise = V_cruise / np.sqrt(R_air * gamma_air * T_isa)
@@ -58,13 +60,13 @@ M_cruise = V_cruise / np.sqrt(R_air * gamma_air * T_isa)
 # Wing
 Cf_wing = 0.455 / (np.log10(Re_wing)**2.58 * (1 + 0.144 * M_cruise**2)**0.65)
 FF_wing = (1 + 0.6 / max_tc_loc_wing * max_tc_wing + 100 * max_tc_wing**4) * \
-          (1.34 * M_cruise**0.18 * (np.cos(lam))**0.28)
+          (1.34 * M_cruise**0.18 * (np.cos(lam_wing))**0.28)
 Swet_wing = 2 * S_wing
 
 # Tail
 Cf_tail = 0.455 / (np.log10(Re_tail)**2.58 * (1 + 0.144 * M_cruise**2)**0.65)
 FF_tail = (1 + 0.6 / max_tc_loc_tail * max_tc_tail + 100 * max_tc_tail**4) * \
-          (1.34 * M_cruise**0.18 * (np.cos(lam))**0.28)
+          (1.34 * M_cruise**0.18 * (np.cos(lam_tail))**0.28)
 Swet_tail = 2 * S_tail  # or use actual tail area if different
 
 # ------------------------------
