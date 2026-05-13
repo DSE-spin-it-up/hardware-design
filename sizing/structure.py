5569ecbded245759e840fe414687dc0e9a9c9b4e
mattiz <mwacornelis@tudelft.nl>
2026-05-13 16:53:06 +0200

    Add compressive failure criterion

--- sizing/structure.py
+++ sizing/structure.py
@@ -28,11 +28,11 @@ def calc_stress(M, t, d):
 
     return sigma
 
-def calc_d(F, L, E, defl):
-    return ((3 * F * L**3) / (np.pi * E * defl))**(1 / 4)
-
-def calc_t(F, L, defl, E, d):
+def calc_t_defl(F, L, defl, E, d):
     return (F * L**3) / (np.pi * E * d**3 * defl)
+
+def calc_t_comp(M, sigma, d):
+    return (4 * M) / (np.pi * sigma * d**2)
 
 def calc_mass(L, d, t, rho):
     return ((d / 2) ** 2 - (d / 2 - t) ** 2) * np.pi * L * rho
@@ -41,19 +41,22 @@ def calc_defl(F, L, E, I):
     return F * L**3 / (8 * E * I)
 
 cfrp = mat.CFRP()
-Y = cfrp.s_t * 0.5
+Y = cfrp.s_c * 0.5
 E = cfrp.E
 rho = cfrp.rho
 sf = 1.2
 sigma_lim = Y / 1.2
-d_max = tw * 0.8
+d_w = tw * 0.8
 defl_max = 0.05
 
 M_w = calc_mom_wing(L, bw)
-d_w = calc_d(L / 2, bw / 2, E, defl_max)
-if d_w > d_max:
-    d_w = d_max
-t_w = calc_t(L / 2, bw / 2, defl_max, E, d_w)
+t_w_defl = calc_t_defl(L / 2, bw / 2, defl_max, E, d_w)
+t_w_comp = calc_t_comp(M_w, Y, d_w)
+t_w = max(t_w_defl, t_w_comp)
+if t_w == t_w_defl:
+    fail_mod = "Maximum allowable deflection exceeded"
+else:
+    fail_mod = "Compressive failure"
 mass_w = calc_mass(bw, d_w, t_w, rho)
 
 defl_w = calc_defl(L / 2, bw / 2, cfrp.E, calc_I(t_w, d_w))
@@ -63,3 +66,4 @@ if __name__ == "__main__":
     print(f"Wing rod outer diameter: {d_w * 1000}mm")
     print(f"Wing rod thickness: {t_w * 1000}mm")
     print(f"Wing rod mass: {mass_w * 1000}g")
+    print(f"Failure model: {fail_mod}")
