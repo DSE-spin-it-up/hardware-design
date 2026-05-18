91ca3641df8435686de95706ad29e19590b4f0f1
slava <v.a.kovalyov@student.tudelft.nl>
2026-05-18 13:57:44 +0200

    Added more props

--- aerodynamics/stability.py
+++ aerodynamics/stability.py
@@ -1,10 +1,17 @@
 
 from aerodynamics.airfoil_polar import AirfoilPolar
 from aerodynamics.llt import WingGeometry
+from sizing.wing import SizingResult
 
+L_tail=SizingResult.L_tail
+ARt=
 
-def calculate_CM_wing(inputs : AirfoilPolar, wing_geometry : WingGeometry) -> float:
+def calculate_CM_wing(inputs : AirfoilPolar, wing_geometry : WingGeometry, ) -> float:
     CM_wing = inputs.Cm * wing_geometry.AR / (2 + wing_geometry.AR)
+
+def calculate_CL_tail(inputs : AirfoilPolar, tail_geometry : WingGeometry) -> float:
+    CL_tail = CM
+    return CL_tail
 
 
 
