9fc9e35801037e406bf775c649f5c95b4dcfa049
mattiz <mwacornelis@tudelft.nl>
2026-05-19 13:48:45 +0200

    Add 21% contingency margin to mass

--- weights/mass.py
+++ weights/mass.py
@@ -257,5 +257,5 @@ def total_mass(
         'glass_sheet_wing': m_sheet_wing,
         'glass_sheet_tail': m_sheet_tail,
         'glass_sheet':      m_glass_sheet,
-        'total':            m_total*1.4,
+        'total':            m_total*1.21,
     }
