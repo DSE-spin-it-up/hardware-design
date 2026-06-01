c3641179f0b705f852bea83e0d4587d42692d697
pholleboom <philip.holleboom@gmail.com>
2026-06-01 11:41:59 +0200

    added rudder sizing

--- sizing/elevator.py
+++ sizing/elevator.py
@@ -6,17 +6,16 @@ import numpy as np
 SE_Sh = 0.25     # range: 0.15 - 0.40
 bE_bh = 0.08     # range: 0.03 - 0.12
 
-elevator_deflection_up = 25     # deg
-elevator_deflection_down = 20   # deg
+elevator_deflection_up = np.radians(25)     # deg
+elevator_deflection_down = np.radians(20)   # deg
 
 dedalpha=(2*CLalpha)/(np.pi*AR) # CLalpha is wing lift curve slope, AR is wing aspect ratio
 epsilon0=(2*CL0)/(np.pi*AR) # CL0 is wing lift coefficient at 0 deg AoA
 epsilon=epsilon0+dedalpha*alpha # alpha is angle of attack in radians
 alphah=alpha+ih-epsilon # ih is horizontal tail incidence angle in radians
 
-CLh=(2*Lh)/(rho*V**2*Sh) # Lh is horizontal tail lift, rho is air density, V is free-stream velocity, Sh is horizontal tail area
 
-taue=(CLh-CLalphah*alphah)/(CLalphah*elevator_deflection_up) # CLalphah is horizontal tail lift curve slope, alphah is horizontal tail angle of attack in radians, elevator_deflection_up is elevator deflection in radians
+taue=(CLh-CLalphah*alphah)/(CLalphah*elevator_deflection_up) # CLh tail lift coefficient, CLalphah is horizontal tail lift curve slope, alphah is horizontal tail angle of attack in radians, elevator_deflection_up is elevator deflection in radians
 
 import numpy as np
 
@@ -34,18 +33,18 @@ def cE_ch(taue):
     # Real roots only
     roots = roots[np.isclose(roots.imag, 0)].real
 
-    # Valid aileron area ratio range
+    # Valid elevator cbord ratio range
     valid = roots[(roots >= 0.2) & (roots <= 0.4)]
 
     if len(valid) == 0:
-        raise ValueError(f"No valid root found for tau_a={tau_a}")
+        raise ValueError(f"No valid root found for taue={taue}")
 
     return valid[0]
 
 
 
 CMdeltaE = -CLalphah * etah * Vh  * bE_bh * taue # etah is the dynamic pressure ratio at the horizontal tail so (tail velocity/freestream velocity)^2, Vh is the horizontal tail volume coefficient
-CLdeltaE = CLalphah * etah * Vh * Sh_S * bE_bh * taue # CLalphah is horizontal tail lift curve slope, etah is dynamic pressure ratio at the horizontal tail, Vh is horizontal tail volume coefficient, bE_bh is elevator span to horizontal tail span ratio, Sh_S is horizontal tail area to wing area ratio
+CLdeltaE = CLalphah * etah * Vh * Sh/S * bE_bh * taue # CLalphah is horizontal tail lift curve slope, etah is dynamic pressure ratio at the horizontal tail, Vh is horizontal tail volume coefficient, bE_bh is elevator span to horizontal tail span ratio, Sh_S is horizontal tail area to wing area ratio
 CLhdeltaE = CLalphah * taue
 
 deltaEcruise = (((T*Z_T)/(q*S*C)+Cm0)*CLalpha+(CLcr-Cl0)*Cmalpha)/(CLalpha*CMdeltaE-Cmalpha*CLdeltaE) # T is thrust, Z_T is vertical distance from thrust action line to which is 2 times wing propeller height plus tail propeller height divided by 3, q is dynamic pressure, S is wing area, C is mean aerodynamic chord, Cm0 is zero-lift pitching moment coefficient, CLcr is lift coefficient during cruise, Cl0 is lift coefficient at 0 deg AoA, Cmalpha is pitching moment slope with respect to angle of attack
--- sizing/rudder.py
+++ sizing/rudder.py
@@ -1,15 +1,97 @@
 # Positive rudder deflection (rudder to the left) yaws nose the left so Cldeltar is -ve.
 
 import numpy as np
+from scipy.optimize import fsolve
 
 # Rudder
 SR_SV = 0.25     # range: 0.15 - 0.35
 cR_CV = 0.28     # range: 0.15 - 0.40
 bR_bV = 0.85     # range: 0.70 - 1.00
 
-rudder_deflection_right = 30    # deg
-rudder_deflection_left = 30     # deg
+rudder_deflection_right = np.radians(30)    # deg
+rudder_deflection_left = np.radians(30)     # deg
 
 Vgust = 5 # m/s, gust velocity for gust load calculations
+CDY = 0.8 # drag coefficient in the y-direction for gust load calculations between 0.5 and 0.8
+Kf1 = 0.85 # correction factor for the fuselage contribution to the control derivative (Cnβ ) and is between (0.65 < Kf1 < 0.85)
+Kf2 = 1.0 # correction factor the fuselage contribution to the control derivative (Cyβ ) and is between (0.75 < Kf2 < 1)
 
 VT = np.sqrt(Vstall**2+Vgust**2) 
+
+beta = np.arctan(Vgust/Vstall) # sideslip angle during gust
+
+Fw = 0.5 * rho * Vgust**2 * Ss * CDY # aerodynamic force due to gust, CDY is the drag coefficient in the y-direction, # Ss is the side area
+
+def equations(x,
+              beta,
+              rho, VT, Vgust, S, b,
+              Cn0, Cnb, Cndr,
+              Cy0, Cyb, Cydr,
+              CDY,
+              Fw, dc):
+
+    sigma, delta_R = x
+
+    eq1 = (
+        0.5 * rho * VT**2 * S * b *
+        (Cn0 + Cnb * (beta - sigma) + Cndr * delta_R)
+        + Fw * np.cos(dc) * np.cos(sigma)
+    )
+
+    eq2 = (
+        0.5 * rho * Vgust**2 * S * CDY
+        - 0.5 * rho * VT**2 * S *
+        (Cy0 + Cyb * (beta - sigma) + Cydr * delta_R)
+    )
+
+    return [eq1, eq2]
+
+
+# Initial guess: [sigma, delta_R]
+x0 = [0.0, 0.0]
+
+Cyb = -Kf2 * CLalphav*etav*Sv/S # CLalphav is vertical tail lift curve slope, dsigma_dbeta is the change in sigma with respect to beta, etav is the dynamic pressure ratio at the vertical tail should be same as for horizontal tail, Sv is vertical tail area, S is wing area
+Cnb = Kf1 * CLalphav*etav*Sv*lv/(b*S) # CLalphav is vertical tail lift curve slope, etav is dynamic pressure ratio at the vertical tail should be same as for horizontal tail, Sv is vertical tail area, bV is vertical tail span, b is wing span, lv is vertical tail arm which is just the tail length 
+Cydr = CLalphav*etav*taur*br/bv*Sr/Sv # CLalphav is vertical tail lift curve slope, etav is dynamic pressure ratio at the vertical tail should be same as for horizontal tail, taur is rudder area to vertical tail area ratio, br is rudder span, bv is vertical tail span, Sr is rudder area, Sv is vertical tail area
+Cndr = - CLalphav*Vv*etav*taur*br/bv # CLalphav is vertical tail lift curve slope, Vv is vertical tail volume coefficient, etav is dynamic pressure ratio at the vertical tail should be same as for horizontal tail, taur is rudder area to vertical tail area ratio, br is rudder span, bv is vertical tail span
+
+sol = fsolve(
+    equations,
+    x0,
+    args=(beta,
+          rho, VT, Vgust, S, b,
+          Cn0, Cnb, Cndr,
+          Cy0, Cyb, Cydr,
+          CDY,
+          Fw, dc)
+)
+
+sigma_sol, delta_R_sol = sol
+# deltaR is the maximal rudder deflection 30 degrees
+print(f"sigma = {sigma_sol}")
+print(f"delta_R = {delta_R_sol}")
+
+
+def cR_cv(taur):
+    coeffs = [
+        -6.624,
+        12.07,
+        -8.292,
+        3.295,
+        0.004942 - taur
+    ]
+
+    roots = np.roots(coeffs)
+
+    # Real roots only
+    roots = roots[np.isclose(roots.imag, 0)].real
+
+    # Valid aileron area ratio range
+    valid = roots[(roots >= 0.2) & (roots <= 0.4)]
+
+    if len(valid) == 0:
+        raise ValueError(f"No valid root found for taur={taur}")
+
+    return valid[0]
+
+# if delta_r obtained from system of equation is less than max required to avoid stall in vertical stab, Cr/Cv must be modified and design parameters must be adjusted accordingly.
