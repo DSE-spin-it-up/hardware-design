25317138c851d6e86d3c8960f20b5be67c586f88
slava <v.a.kovalyov@student.tudelft.nl>
2026-06-16 19:31:56 +0200

    Refactor motor parameters for improved calculations and update output format

--- propulsion/motor.py
+++ propulsion/motor.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-KV = 350 #350 477
-n = 13375 #8550 11300 13375
+KV = 700 #350 477
+n = 10000 #8550 11300 13375
 V_emf = n / KV
-Q = 1.02
-R_w = 40/1000 # 40 84
-I0 = 1.1 # 1.1 0.96
+Q = 0.6
+R_w = 33/1000 # 40 84
+I0 = 3.4 # 1.1 0.96
 Kt = 60 / (2 * np.pi * KV)
 I = (Q / Kt) + I0
 V = I * R_w + V_emf
-
-print(I)
 print(V)
-print(V * I)
+print(I)
+
+print(V/24)
